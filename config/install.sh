9255cc742adcf64db580ab40b4adfabb4aca7a04
Andrew Ghostuhin <me@torinasakura.name>
2024-11-22 16:31:36 +0300

    feat(env): local stage upgrade

--- config/install.sh
+++ config/install.sh
@@ -2,55 +2,58 @@
 
 set -e
 
-mysql_ready="nc -z $WORDPRESS_DB_HOST 3306"
+# Проверяем доступность MySQL
+while ! mysqladmin ping -h"$WORDPRESS_DB_HOST" --silent; do
+  echo 'Waiting for database...'
+  sleep 1
+done
 
-if ! $mysql_ready
-then
-    printf 'Waiting for MySQL.'
-    while ! $mysql_ready
-    do
-        printf '.'
-        sleep 1
-    done
-    echo
+# Проверяем, установлен ли WordPress
+if wp core is-installed; then
+  echo "WordPress is already installed, exiting."
+  exit 0
 fi
 
-if wp core is-installed
-then
-    echo "WordPress is already installed, exiting."
-    exit
-fi
+# Скачиваем WordPress
+wp core download --force --version="${WORDPRESS_VERSION:-latest}"
 
-wp core download --force --version=5.4
-
+# Создаём конфигурацию, если её нет
 [ -f wp-config.php ] || wp config create \
-    --dbhost="$WORDPRESS_DB_HOST" \
-    --dbname="$WORDPRESS_DB_NAME" \
-    --dbuser="$WORDPRESS_DB_USER" \
-    --dbpass="$WORDPRESS_DB_PASSWORD"
+  --dbhost="$WORDPRESS_DB_HOST" \
+  --dbname="$WORDPRESS_DB_NAME" \
+  --dbuser="$WORDPRESS_DB_USER" \
+  --dbpass="$WORDPRESS_DB_PASSWORD"
 
+# Устанавливаем WordPress
 wp core install \
-    --url="$WORDPRESS_URL" \
-    --title="$WORDPRESS_TITLE" \
-    --admin_user="$WORDPRESS_ADMIN_USER" \
-    --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
-    --admin_email="$WORDPRESS_ADMIN_EMAIL" \
-    --skip-email
+  --url="$WORDPRESS_URL" \
+  --title="$WORDPRESS_TITLE" \
+  --admin_user="$WORDPRESS_ADMIN_USER" \
+  --admin_password="$WORDPRESS_ADMIN_PASSWORD" \
+  --admin_email="$WORDPRESS_ADMIN_EMAIL" \
+  --skip-email
 
-cp -r /opt/yootheme /var/www/html/wp-content/themes
-cp -r /opt/default-theme /var/www/html/wp-content/themes
+# Копируем темы, если они существуют
+[ -d /opt/yootheme ] && cp -r /opt/yootheme /var/www/html/wp-content/themes
+[ -d /opt/default-theme ] && cp -r /opt/default-theme /var/www/html/wp-content/themes
 
+# Активируем тему
 wp theme activate yootheme
-wp theme delete twentysixteen twentyseventeen twentynineteen twentytwenty
 
+# Удаляем стандартные темы
+wp theme list --field=name | grep -E 'twenty' | xargs -I {} wp theme delete {}
+
+# Отключаем автообновления
 wp config set WP_AUTO_UPDATE_CORE false
 
+# Удаляем стандартные плагины и устанавливаем свои
 wp plugin delete akismet hello
-wp plugin install --force --activate https://github.com/polylang/polylang/archive/2.7.1.zip
-wp plugin install --force --activate https://downloads.wordpress.org/plugin/2fas-light.zip
-wp plugin install --force --activate https://github.com/Yoast/wordpress-seo/archive/13.4.zip
+wp plugin install --force --activate polylang
+wp plugin install --force --activate wordpress-seo
 
-wp term delete category 1
-wp post delete 1
+# Удаляем стандартную категорию и пост
+wp term get category 1 && wp term delete category 1 || true
+wp post get 1 && wp post delete 1 || true
 
+# Выводим сообщение об успешной установке
 echo "Great. You can now log into WordPress at: $WORDPRESS_URL/wp-admin"
